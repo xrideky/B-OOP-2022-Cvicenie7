dcfb19b0723d822c01cba05ff8865b2c219572f3
xrideky <xrideky@stuba.sk>
2022-04-07 20:19:58 +0200

    !adapter! HELPFUL

--- src/main/java/sk/stuba/fei/uim/oop/Okno.java
+++ src/main/java/sk/stuba/fei/uim/oop/Okno.java
@@ -1,88 +1,22 @@
 package sk.stuba.fei.uim.oop;
 
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.MouseEvent;
 
-
-public class Okno extends JFrame implements MouseListener, MouseMotionListener, KeyListener, ChangeListener {
+public class Okno extends UniversalAdapter {
     public Okno() throws HeadlessException {
         super();
         this.setSize(400,400);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-       // this.addMouseListener(this);
-       // this.addMouseMotionListener(this);
-
-        // this.addKeyListener(this);
-
-        JSlider s = new JSlider(JSlider.HORIZONTAL,0,50,20);
-        s.setMinorTickSpacing(5);
-        s.setMajorTickSpacing(10);
-        s.setPaintTicks(true);
-        s.setPaintLabels(true);
-
-        s.addChangeListener(this);
-        this.add(s);
+        this.addMouseListener(this);
 
         this.setVisible(true); //malo by to byt na konci pri inicializacii
     }
 
-
     @Override
     public void mouseClicked(MouseEvent e) {
         System.out.println("KLIK " + e.getPoint().toString());
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-        System.out.println("STLACENE " + e.getPoint().toString());
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        System.out.println("NESTLACENE " + e.getPoint().toString());
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        System.out.println("VSTUP " + e.getPoint().toString());
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-        System.out.println("VYSTUP " + e.getPoint().toString());
-    }
-
-    @Override
-    public void mouseDragged(MouseEvent e) {
-        System.out.println("TAHAS " + e.getPoint().toString());
-    }
-
-    @Override
-    public void mouseMoved(MouseEvent e) {
-        System.out.println("POHNUTIE " + e.getPoint().toString());
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-        System.out.println("Stlacene " + e.getKeyChar());
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        System.out.println("Stlacil si " + e.getKeyChar());
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        System.out.println("Nestlacene " + e.getKeyChar());
-    }
-
-    @Override
-    public void stateChanged(ChangeEvent e) {
-        System.out.println(((JSlider) e.getSource()).getValue());
     }
 }
